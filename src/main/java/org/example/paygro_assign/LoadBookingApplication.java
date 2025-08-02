272db99d56bcafe310321138d6055e9d4d2a939c
harshans22 <harshhub.414@gmail.com>
2025-08-02 22:32:30 +0530

    added docker

--- src/main/java/org/example/paygro_assign/LoadBookingApplication.java
+++ src/main/java/org/example/paygro_assign/LoadBookingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoadBookingApplication {
+    // This is the main entry point for the Load Booking application.
     public static void main(String[] args) {
         SpringApplication.run(LoadBookingApplication.class, args);
     }
